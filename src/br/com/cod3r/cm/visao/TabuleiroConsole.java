a7c0dce120b54f5193ab73b81c7e4880f932257c
Sub1M <viniciusgmazzi@gmail.com>
2024-02-22 13:54:09 -0300

    Modificação

--- src/br/com/cod3r/cm/visao/TabuleiroConsole.java
+++ src/br/com/cod3r/cm/visao/TabuleiroConsole.java
@@ -38,7 +38,7 @@ public class TabuleiroConsole {
     		
     	}
     } catch (SairException e) {
-    	System.out.println("Tchau!!");
+    	System.out.println("Tchau!");
     } finally {
 		entrada.close();
 	}
